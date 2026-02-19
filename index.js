83d8750aa9badd60cb3b17911a70c6d105bc8077
ykarfaridis <ykarfaridis@gmail.com>
2026-02-18 19:50:45 -0600

    Final Draft

--- index.js
+++ index.js
@@ -4,6 +4,20 @@
 
 let contrastToggle = false
 
+const scaleFactor = 1/20;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape")
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 :1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+}
+
 function toggleContrast () {
     contrastToggle = !contrastToggle;
     if (contrastToggle){
